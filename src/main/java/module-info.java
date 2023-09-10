97ee4f5cac1bdafbf536e880f6705c025cbea3df
Tan Chee Heng <ch.tan020@gmail.com>
2023-09-10 15:26:40 +0800

    Fix classes loaded from unnamed module error

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+//module-info.java configuration syntax inspired by https://www.baeldung.com/java-9-modularity
+module jude {
+    requires javafx.base;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.controls;
+}
